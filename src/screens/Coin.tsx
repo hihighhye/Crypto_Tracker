64e59b5fd452a2949e730ce8774f456a2278f85c
hihighhye <hjkim.builton@gmail.com>
2025-06-01 12:49:51 +0900

    Chapter #5.4

--- src/screens/Coin.tsx
+++ src/screens/Coin.tsx
@@ -1,13 +1,44 @@
-import { useParams } from "react-router-dom";
+import { useState } from "react";
+import { useLocation, useParams } from "react-router-dom";
+import styled from "styled-components";
 
-interface coinIdProps {
-    coinId: string;
-}
+
+const Container = styled.div`
+    padding: 0px 20px;
+    max-width: 470px;
+    margin: 0 auto;
+`;
+
+const Header = styled.header`
+    height: 10vh;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    margin: 20px 0px;
+`;
+
+const Title = styled.h1`
+    font-size: 48px;
+    color: ${props => props.theme.accentColor};
+`;
+
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
 
 function Coin() {
     const {coinId} = useParams();
+    const [loading, setLoading] = useState(true);
+    const {state} = useLocation();
     return (
-        <h1>Coin: {coinId}</h1>
+        <Container>
+            <Header>
+                <Title>{state?.name || "Loading..."}</Title>    
+            </Header>
+            {loading ? (<Loader>Loading...</Loader>) : null}
+        </Container>
     );
 }
 
--- src/screens/Coins.tsx
+++ src/screens/Coins.tsx
@@ -1,8 +1,11 @@
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 const Container = styled.div`
     padding: 0px 20px;
+    max-width: 470px;
+    margin: 0 auto;
 `;
 
 const Header = styled.header`
@@ -10,6 +13,7 @@ const Header = styled.header`
     display: flex;
     justify-content: center;
     align-items: center;
+    margin: 20px 0px;
 `;
 
 const CoinsList = styled.ul``;
@@ -18,10 +22,11 @@ const Coin = styled.li`
     background-color: white;
     color: ${(props) => props.theme.bgColor};
     border-radius: 15px;
-    margin-bottom: 10px;
+    margin: 10px 0px;
     a {
+        display: flex;
+        align-items: center;
         transition: color 0.2s ease-in;
-        display: block;
         padding: 20px;
     }
     &:hover {
@@ -32,51 +37,65 @@ const Coin = styled.li`
 `;
 
 const Title = styled.h1`
-font-size: 48px;
+    font-size: 48px;
     color: ${props => props.theme.accentColor};
 `;
 
-const coins = [
-    {
-        id: "btc-bitcoin",
-        name: "Bitcoin",
-        symbol: "BTC",
-        rank: 1,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "eth-ethereum",
-        name: "Ethereum",
-        symbol: "ETH",
-        rank: 2,
-        is_new: false,
-        is_active: true,
-        type: "coin",
-    },
-    {
-        id: "hex-hex",
-        name: "HEX",
-        symbol: "HEX",
-        rank: 3,
-        is_new: false,
-        is_active: true,
-        type: "token",
-    },
-];
+const Loader = styled.span`
+    text-align: center;
+    display: block;
+`;
+
+const Img = styled.img`
+    width: 35px;
+    height: 35px;
+    margin-right: 10px;
+`;
+
+interface CoinInterface {
+    id: string,
+    name: string,
+    symbol: string,
+    rank: number,
+    is_new: boolean,
+    is_active: boolean,
+    type: string,
+}
 
 function Coins() {
+    const [coins, setCoins] = useState<CoinInterface[]>([]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        (async() => {
+            const json = await (
+                await fetch("https://api.coinpaprika.com/v1/coins")
+            ).json();
+            setCoins(json.slice(0, 100));
+            setLoading(false);
+            console.log(coins);
+        })();
+        
+    }, [])
     return (
         <Container>
             <Header>
                 <Title>Coins</Title>    
             </Header>
-            <CoinsList>
-                {coins.map(coin => (
-                    <Coin key={coin.id}><Link to={`/${coin.id}`}>{coin.name} &rarr;</Link></Coin>
-                ))}
-            </CoinsList>
+            {loading ? (<Loader>Loading...</Loader>) : 
+                (<CoinsList>
+                    {coins.map(coin => (
+                        <Coin key={coin.id}>
+                            <Link to={`/${coin.id}`} state={{"name": coin.name}}>
+                                <Img 
+                                    src={`https://cryptocurrencyliveprices.com/img/${coin.id}.png`} 
+                                />
+                                {coin.name} &rarr
+                            </Link>
+                        </Coin>
+                    ))}
+                </CoinsList>)
+            }
         </Container>
     );
 }
