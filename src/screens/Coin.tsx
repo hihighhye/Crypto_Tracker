ca968be6371cd7747cda63eb823dfaaf634dc380
hihighhye <hjkim.builton@gmail.com>
2025-06-01 15:27:32 +0900

    Chapter #5.6

--- src/screens/Coin.tsx
+++ src/screens/Coin.tsx
@@ -42,13 +42,68 @@ const LinkMain = styled.span`
 
 `;
 
+interface InfoData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    is_new: boolean;
+    is_active: boolean;
+    type: string;
+    logo: string;
+    description: string;
+    message: string;
+    open_source: boolean;
+    started_at: string;
+    development_status: string;
+    hardware_wallet: boolean;
+    proof_type: string;
+    org_structure: string;
+    hash_algorithm: string;
+    first_data_at: string;
+    last_data_at: string;
+}
+
+interface PriceData {
+    id: string;
+    name: string;
+    symbol: string;
+    rank: number;
+    total_supply: number;
+    max_supply: number;
+    beta_value: number;
+    first_data_at: string;
+    last_updated: string;
+    quotes: {
+        USD: {
+            price: string;
+            volume_24h: number;
+            volume_24h_change_24h: number;
+            market_cap: number;
+            market_cap_change_24h: number;
+            percent_change_15m: number;
+            percent_change_30m: number;
+            percent_change_1h: number;
+            percent_change_6h: number;
+            percent_change_12h: number;
+            percent_change_24h: number;
+            percent_change_7d: number;
+            percent_change_30d: number;
+            percent_change_1y: number;
+            ath_price: number;
+            ath_date: number;
+            percent_from_price_ath: number;
+        }
+    }
+
+}
 
 function Coin() {
     const {coinId} = useParams();
     const [loading, setLoading] = useState(true);
     const {state} = useLocation();
-    const [info, setInfo] = useState({});
-    const [priceInfo, setPriceInfo] = useState({});
+    const [info, setInfo] = useState<InfoData>();
+    const [priceInfo, setPriceInfo] = useState<PriceData>();
     useEffect(() => {
         (async () => {
             const infoData = await (
@@ -57,6 +112,8 @@ function Coin() {
             const priceData = await (
                 await fetch(`https://api.coinpaprika.com/v1/tickers/${coinId}`)
             ).json();
+            setInfo(infoData);
+            setPriceInfo(priceData);
         })();
     }, []);
     return (
