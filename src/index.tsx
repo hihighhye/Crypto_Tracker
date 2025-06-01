2a66df1ce9f0f83377a3e12075594cf410325a4c
hihighhye <hjkim.builton@gmail.com>
2025-06-01 10:36:40 +0900

    Chapter #5.1

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import Router from './Router';
 import { RouterProvider } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
+import {theme} from "./theme";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
-    <RouterProvider router={Router} />
+    <ThemeProvider theme={theme}>
+      <RouterProvider router={Router} />
+    </ThemeProvider>
   </React.StrictMode>
 );
 
